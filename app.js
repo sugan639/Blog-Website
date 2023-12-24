17dd6b2d8f2bc68512c4ef65a44d35c43ce4ca86
sugan639 <sugankarthick1309@gmail.com>
2023-12-24 13:07:20 +0530

    added_route_parameters

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to Sugan Blogs! Immerse yourself in the captivating tales of globetrotters as we explore the world's most enchanting destinations. From hidden gems to iconic landmarks, our blog is your go-to guide for travel inspiration, insider tips, and unforgettable experiences. Join us on a journey to discover the beauty of diverse cultures, savor mouthwatering cuisines, and uncover the secrets of each corner of the globe. Let the wanderlust begin – your next extraordinary adventure awaits!";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -49,14 +49,14 @@ app.post("/compose",function(req,res){
 
 app.get("/posts/:request",function(req,res){
   const query = _.lowerCase( req.params.request) ;
-  for(var i=0; i<posts.length;i++){
-    if(posts[i].title == query){
+  posts.forEach(function(element){
+    if(element.title == query){
     console.log("Match found!");
     }
     else{
       console.log("Match Not found!");
     }
-  }
+  })
 })
 
 
